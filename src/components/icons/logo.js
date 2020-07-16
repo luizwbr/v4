391a46752303f2213a9a6b83409f90a54ed9565b
Luiz Felipe Weber <luiz.weber@wssim.com.br>
2020-07-15 23:47:55 -0300

    Atualizando logo

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -4,10 +4,10 @@ const IconLogo = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="79" height="91.154" viewBox="0 0 79 91.154">
     <g id="Logo" transform="translate(-10.5 -4.423)">
       <g id="Grupo_1" data-name="Grupo 1" transform="translate(11 5)">
-        <g id="Componente_1_1" data-name="Componente 1 – 1" transform="translate(22 22)">
-          <text id="W" fill="#0076a8" font-size="36" font-family="Roboto-Bold, Roboto" font-weight="700"><tspan x="1.259" y="38">W</tspan></text>
+        <g id="Componente_1_1" data-name="Componente 1 – 1" transform="translate(19 21)">
+          <text id="W" transform="translate(1.259 38)" fill="#0076a8" font-size="39" font-family="Arial-BoldMT, Arial" font-weight="700"><tspan x="0" y="0">W</tspan></text>
         </g>
-        <path id="Shape" d="M39,0,0,22V67L39,90,78,68V23Z" stroke="#0076a8" stroke-width="1"/>
+        <path id="Shape" d="M39,0,0,22V67L39,90,78,68V23Z" fill="none" stroke="#0076a8" stroke-width="1"/>
       </g>
     </g>
   </svg>
